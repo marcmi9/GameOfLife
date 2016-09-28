8a2ed41086d90370f15f37d39183e96a130ed04b
Marc <marcmi@github.com>
2016-09-28 17:51:59 +0200

    Added window improvements

--- build/main_GoL.c
+++ build/main_GoL.c
@@ -9,7 +9,7 @@
 
 #define rows 20
 #define cols 20
-#define init_center 10  //referencia inicial
+//#define init_center 10  //referencia inicial
 #define DELAY 10000*50 //que es¿?¿? --> UN DELAY PERQUÈ NO S'EXECUTI TOT INSTANTÀNIAMENT
 
 void store_cells(int cell_list[rows*cols][2], int * n_cells, char live_cells[rows+2][cols+2]);
@@ -18,12 +18,22 @@ int count_cells(int x, int y, char live_cells[rows+2][cols+2]);
 
 void plot_cells(char live_cells[rows+2][cols+2]);
 
+WINDOW *init_window(int height, int width);
+
+WINDOW *my_win;
+
+//int cols = 2*rows;
+int init_center_x = cols/2;
+int init_center_y = rows/2;
+
 int main()
 {    
 	
 	initscr();
 	noecho();
 	curs_set(FALSE);
+	
+	my_win = init_window(rows,cols);
 	
     int cell_list[rows*cols][2]; // llista amb les coordenades de les cells vives
     memset(cell_list,0,sizeof(int)*(rows)*(cols)*2); //inicialitzem a 0. 
@@ -45,28 +55,28 @@ int main()
     memset(checked_cells,0,sizeof(char)*(rows+2)*(cols+2)); //inicialitzem a 0
 
     //------------------------------------ init -----------------------------------------
-    cell_list[0][0] = init_center;
-    cell_list[0][1] = init_center;
+    cell_list[0][0] = init_center_x;
+    cell_list[0][1] = init_center_y;
 
     n_cells++;
 
-    cell_list[1][0] = init_center;
-    cell_list[1][1] = init_center+1;
+    cell_list[1][0] = init_center_x;
+    cell_list[1][1] = init_center_y+1;
 
     n_cells++;
 
-    cell_list[2][0] = init_center;
-    cell_list[2][1] = init_center-1;
+    cell_list[2][0] = init_center_x;
+    cell_list[2][1] = init_center_y-1;
+	
+    n_cells++;
+
+    cell_list[3][0] = init_center_x-1;
+    cell_list[3][1] = init_center_y;
 
     n_cells++;
 
-    cell_list[3][0] = init_center-1;
-    cell_list[3][1] = init_center;
-
-    n_cells++;
-
-    cell_list[4][0] = init_center+1;
-    cell_list[4][1] = init_center;
+    cell_list[4][0] = init_center_x+1;
+    cell_list[4][1] = init_center_y;
 
     n_cells++;
     //---------------------------------------------------------------------------------------------
@@ -76,7 +86,7 @@ int main()
     plot_cells(live_cells);
     
     //-------------------------------------- programa principal ---------------------------------------------------
-    for(int l = 0; l < 5; l++)
+    for(int l = 0; true; l++)
     {
 
         for (int i = 0; i < n_cells; i++) //Per a cada cell viva s'evaluen els neighbours
@@ -136,8 +146,19 @@ int main()
     return 0;
 }
 
+WINDOW *init_window(int height, int width) {
+	
+	WINDOW *wind;
+	wind = newwin(height, width,0,0);
+	
+	wrefresh(wind);
+	
+	return wind;
+}
+
 void store_cells(int cell_list[rows*cols][2], int * n_cells, char live_cells[rows+2][cols+2])
-{ //Passa de cell_list a live_cells
+{ 
+	//Passa de cell_list a live_cells
     for (int i = 0; i < *n_cells; i++)
     {
         live_cells[cell_list[i][1]][cell_list[i][0]] = 1;
@@ -146,7 +167,7 @@ void store_cells(int cell_list[rows*cols][2], int * n_cells, char live_cells[row
 
 void check_cell(int x, int y, char live_cells[rows+2][cols+2], char checked_cells[rows+2][cols+2], int cell_list_2[rows*cols][2], int * n_cells_2 ,char live_cells_2[rows+2][cols+2])
 {
-//    printf("%p\n", &live_cells[0][0]);
+
 
     int neighbours;
 
@@ -191,23 +212,20 @@ int count_cells(int x, int y, char live_cells[rows+2][cols+2])
 
 void plot_cells(char live_cells[rows+2][cols+2])
 {
-	clear();
+	wclear(my_win);
+	box(my_win,0,0);
+	wborder(my_win, '|', '|', '-', '-', '+', '+', '+', '+');
     for (int i = 1; i <= rows; i++)
     {
         for (int j = 1; j <= cols; j++)
         {
-            //printf("%i ", live_cells[j][i]);
-            char hola[1];
-            strcpy(hola, "a");
             if (live_cells[j][i] == 1) {
-				mvprintw(j,i,"O"); //Not working....
+				mvwprintw(my_win,j,i,"O"); //Not working....
 			}
         }
-        //printf("\n");
     }
-    refresh();
-    usleep(DELAY);
-    //printf("\n");
+    wrefresh(my_win);
+    usleep(DELAY); 
 }
 
 
