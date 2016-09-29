3f943229eb3d72f9d7a5fb52c38c03a12b90b1e0
Marc <marcmi@github.com>
2016-09-29 12:43:39 +0200

    Minor issues

--- build/main_GoL.c
+++ build/main_GoL.c
@@ -4,16 +4,15 @@
 #include <ncurses.h>
 #include <unistd.h>
 
-//X cap a la dreta, Y cap abaix --> ncurses funciona al revés...
+// X CAP A BAIX, Y CAP A LA DRETA!
 
 /* COSES QUE FALTEN:
- * - Canviar eixos X per Y.
- * - Si canviem numero de cols de 20 a 40 (per exemple) la meitat del dibuix no es veu! per què?
+ * - Si el número de cols i rows és diferent la meitat del dibuix no es veu! per què?
  */
 
 
-#define rows 40
-#define cols 40	
+#define rows 80
+#define cols 80	
 #define DELAY 5000 //que es¿?¿? --> UN DELAY PERQUÈ NO S'EXECUTI TOT INSTANTÀNIAMENT
 
 void store_cells(int cell_list[rows*cols][2], int * n_cells, char live_cells[rows+2][cols+2]);
@@ -173,7 +172,7 @@ int starting_menu() {
     int choice = -1;
 
     WINDOW *w;
-    char list[12][18] = { "Select (with ->):", "The R-Pentomino", "Diehard", "Acorn", "Glider", "Small Exploder", "Exploder", "10_cell_row", "Spaceship", "Tumbler", "Glider Gun", "Quit" };
+    char list[12][18] = { "SELECT (WITH ->):", "The R-Pentomino", "Diehard", "Acorn", "Glider", "Small Exploder", "Exploder", "10_cell_row", "Spaceship", "Tumbler", "Glider Gun", "QUIT" };
     char item[18];
     int ch, i = 0;
 
@@ -197,8 +196,9 @@ int starting_menu() {
     curs_set( 0 ); // hide the default screen cursor.
      
        // get the input
-    while(choice == -1 && ( ch = wgetch(w)) != 'q'){ 
-         
+    while (choice == -1){ 
+			
+			ch = wgetch(w);
                 // right pad with spaces to make the items appear with even width.
             sprintf(item, "%-15s",  list[i]); 
             mvwprintw( w, i+1, 2, "%s", item ); 
@@ -217,11 +217,11 @@ int starting_menu() {
 					break;
             }
             // now highlight the next item in the list.
-            wattron( w, A_STANDOUT );
+            wattron( w, A_STANDOUT | A_BOLD);
              
             sprintf(item, "%-15s",  list[i]);
             mvwprintw( w, i+1, 2, "%s", item);
-            wattroff( w, A_STANDOUT );
+            wattroff( w, A_STANDOUT | A_BOLD );
     }
  
     delwin( w );
@@ -235,11 +235,12 @@ void hints() {
 	
     WINDOW *w;
 
-    w = newwin( 4, 25, 1, cols + 10 ); // create a new window
+    w = newwin( 5, 25, 1, cols + 10 ); // create a new window
     box( w, 0, 0 ); // sets default borders for the window
     
     mvwprintw(w, 1, 2, "Press 'D' to advance");
     mvwprintw(w, 2, 2, "Press 'Q' to quit");
+    mvwprintw(w, 3, 2, "Press 'R' to return");
 	
 	wrefresh(w);
 	refresh();
@@ -474,6 +475,7 @@ void create_object(int x, int y, int choice, int cell_list[rows*cols][2], int *n
         break;
 
         case 10:
+        
 			// glider_gun
             draw_cell(x  , y+2, cell_list, n_cells);
             draw_cell(x  , y+2, cell_list, n_cells);
@@ -516,8 +518,9 @@ void create_object(int x, int y, int choice, int cell_list[rows*cols][2], int *n
             draw_cell(x+35, y+8, cell_list, n_cells);
             draw_cell(x+37, y+8, cell_list, n_cells);
             draw_cell(x+35, y+9, cell_list, n_cells);
-
+		
         break;
+        
 
         default:
         break;
